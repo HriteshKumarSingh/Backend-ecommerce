7693d5b086b96d7b5f75d61d4c6f7029dcef61ed
Hritesh Kumar Singh <2003hriteshkumarsingh@gmail.com>
2024-09-10 21:02:06 +0530

    Add database connection

---
+++ src/app.js
@@ -0,0 +1,24 @@
+import "dotenv/config"
+import cors from "cors"
+import cookieParse from "cookie-parser"
+import express, { json } from "express"
+
+
+const app = express()
+
+app.use(cors({
+    origin : process.env.CORS_ORIGIN,
+    credentials : true
+}))
+
+app.use(express.json({limit : "16kb"}))
+app.use(express.urlencoded({extended : true, limit : "16kb"}))
+app.use(express.static("public"))
+app.use(cookieParse())
+
+
+
+
+
+
+export {app}
